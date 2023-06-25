297aa254e1580a6fce2c4e8da037fe6298acf446
R4M45H <R4M45H@users.noreply.github.com>
2023-06-25 19:01:07 +0300

    added TestBoxTests

---
+++ src/test/java/demoqa/TestBoxTests.java
@@ -0,0 +1,42 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest(){
+        String userName = "Ramaw One";
+        String userEmail = "RamawOne@gmail.com";
+        String currentAddress = "Kudrovo, Evrop.pr. 8";
+        String permanentAddress = "Ulica Ququeva";
+
+        open("/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $("#output").shouldBe(visible);
+        $("#output").$("#name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+
+    }
+}
